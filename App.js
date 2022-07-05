6ff4e65c8ea16c0af6763c986041987807c1e112
dianaTSmaz <92556915+dianaTSmaz@users.noreply.github.com>
2022-07-05 13:28:36 -0600

    Add files via upload
     Realicé diversas modificaciones, principalmente en package.json , ya que instalé @babel/preset-env y modifiqué .browserlist del mismo documento...
    Para tratar de solucionar el problema de transpiling seguí los pasos de la siguiente documentación https://docs.mapbox.com/mapbox-gl-js/guides/install/#transpiling
    sin embargo no funcionó.
    También se realizaron cambios en App.js, pero de igual forma se sigue mostrando el pin, pero el mapa no se carga.

---
+++ App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Map,{Marker} from "react-map-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './App.css';
+
+
+
+function App() {
+
+    const token ="pk.eyJ1IjoiZGlhbmEwMjEwIiwiYSI6ImNsNTdhbmY1NjFxbHAzY3FxZDF0bXVwYTEifQ.cZiBDPYd-SibVrMhuHlqWA";
+  return (
+    <div>
+      <h1>This map</h1>
+      <Map
+        initialViewState={{
+          latitude: 37.8,
+          longitude: -122.4,
+          zoom: 14
+        }}
+        style={{ width: 800, height: 600 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={token}
+      >
+        <Marker longitude={-122.4} latitude={37.8} color="red" />
+      </Map>
+   
+    </div>
+  );
+}
+
+export default App;
